32f1bfe3a0cb98733c5ad877466fb63df8996604
Rachel Kelley <rachakelley@gmail.com>
2017-11-29 17:06:36 -0500

    fixed indentation

--- Driver.java
+++ Driver.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Driver {
 	
 	public static void main(String[] args) {
-		
-	Scanner keyIn = new Scanner(System.in);
+		Scanner keyIn = new Scanner(System.in);
         System.out.println("Warshall's Algorithm\n"
         		+ "---------------------");
         System.out.println("Enter the size n of the set\n");
@@ -18,22 +17,22 @@ public class Driver {
         
         System.out.println();
         // Displaying the beginning matrix. (unchanged matrix input)
-	System.out.println("W0 =");
-	display(graph);
-	    
-	// Warshall's algorithm for finding the transitive closure.
-	// Prints all intermediate matrices W1 through Wn.
-        for(int count = 0; count < graph.length; count++) {
-		for(int row = 0; row < graph.length; row++)
-		    for(int col = 0; col < graph.length; col++)
-		    	if(graph[row][count] == 1 && graph[count][col] == 1)
-		    		graph[row][col] = 1;
-		   
-		if(count == graph.length - 1)
-		  	System.out.println("W" + (count + 1) + " = W* =");
-		else
-		 	System.out.println("W" + (count + 1) + " =");
+		System.out.println("W0 =");
 		display(graph);
+	    
+		// Warshall's algorithm for finding the transitive closure.
+		// Prints all intermediate matrices W1 through Wn.
+		for(int count = 0; count < graph.length; count++) {
+			for(int row = 0; row < graph.length; row++)
+				for(int col = 0; col < graph.length; col++)
+					if(graph[row][count] == 1 && graph[count][col] == 1)
+						graph[row][col] = 1;
+		   
+			if(count == graph.length - 1)
+				System.out.println("W" + (count + 1) + " = W* =");
+			else
+				System.out.println("W" + (count + 1) + " =");
+			display(graph);
 	    }
 	}
 
@@ -50,7 +49,7 @@ public class Driver {
 	       		if (graph[col][row] != 0) 
 	        		System.out.print(" 1 ");
 	        	else                  
-	                	System.out.print(" 0 ");
+	                System.out.print(" 0 ");
 	        }
 	        System.out.println();
 	    }
